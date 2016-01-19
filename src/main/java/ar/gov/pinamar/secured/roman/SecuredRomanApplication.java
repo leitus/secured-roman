b2f22c112430413e53cd7815d11a88e688691eaf
leitus <chiquitonavarro@gmail.com>
2016-01-19 07:55:45 -0300

    First Commit

---
+++ src/main/java/ar/gov/pinamar/secured/roman/SecuredRomanApplication.java
@@ -0,0 +1,12 @@
+package ar.gov.pinamar.secured.roman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredRomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredRomanApplication.class, args);
+	}
+}
